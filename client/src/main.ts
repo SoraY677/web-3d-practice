7d2de2ed8ce8832ed81b781b72eb1af1a3166a3e
soray677 <soray677@outlook.com>
2023-04-23 14:19:48 +0900

    カメラ位置調整

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,82 +1,83 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.lookAt(new THREE.Vector3(1, 1, 1))
+camera.position.set(1, 2, 6)
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
+renderer.outputEncoding = THREE.sRGBEncoding
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
-renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild( renderer.domElement );
-
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
 
 let clickTargetId: string | null = null
 document.body.addEventListener('mousemove', (event) => {
-  const element = event.currentTarget as HTMLBodyElement;
-  const x = event.clientX - element.offsetLeft;
-  const y = event.clientY - element.offsetTop;
-  const w = element.offsetWidth;
-  const h = element.offsetHeight;
-  mouse.x = (x/w)*2-1;
-  mouse.y = -(y/h)*2+1;
+  const element = event.currentTarget as HTMLBodyElement
+  const x = event.clientX - element.offsetLeft
+  const y = event.clientY - element.offsetTop
+  const w = element.offsetWidth
+  const h = element.offsetHeight
+  mouse.x = (x / w) * 2 - 1
+  mouse.y = -(y / h) * 2 + 1
 })
 document.body.addEventListener('click', () => {
-  if(!clickTargetId) return;
+  if (!clickTargetId) return
 
   alert(clickTargetId)
 })
 
-
 const loader = new GLTFLoader()
-loader.load( './model/coffee_demo.gltf', function ( gltf ) {
-  const material = new THREE.MeshStandardMaterial();
-  gltf.scene.traverse( function( n) {
-     const node = n as THREE.Mesh
-    if ( node.isMesh ) {
-      node.material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
-    }
-  } );
-  material.color.set( 0xff0000 ); // 赤色
+loader.load(
+  './model/coffee_demo.gltf',
+  function (gltf) {
+    const material = new THREE.MeshStandardMaterial()
+    gltf.scene.traverse(function (n) {
+      const node = n as THREE.Mesh
+      if (node.isMesh) {
+        node.material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+      }
+    })
+    material.color.set(0xff0000) // 赤色
 
-  scene.add( gltf.scene )
-}, function() {
-}, function ( error ) {
-	console.error( error );
-} );
+    scene.add(gltf.scene)
+    renderer.render(scene, camera)
+    document.body.appendChild(renderer.domElement)
+  },
+  function () {
+    animate()
+  },
+  function (error) {
+    console.error(error)
+  }
+)
 
-
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 0.5, 0 );
-controls.update();
-controls.enablePan = false;
-controls.enableDamping = true;
-
-camera.position.z = 5;
-
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0, 0.5, 0)
+controls.update()
+controls.enablePan = false
+controls.enableDamping = true
 
 function animate() {
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate)
 
-  if(! scene.children?.[0]?.children) return
+  if (!scene.children?.[0]?.children) return
 
-  raycaster.setFromCamera(mouse,camera);
-  const intersects = raycaster.intersectObjects(scene.children[0].children, false);
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObjects(scene.children[0].children, false)
 
-  if(intersects.length > 0){
-      const obj = intersects[0].object;
-      clickTargetId = obj.name
+  if (intersects.length > 0) {
+    const obj = intersects[0].object
+    clickTargetId = obj.name
   } else {
     clickTargetId = null
   }
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera)
 }
-
-animate();
